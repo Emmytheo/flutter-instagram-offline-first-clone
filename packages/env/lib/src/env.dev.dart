11499c92e5c6a6123b309618e398b8d2e6d6aee1
Emmytheo <chidi.mgbara@gmail.com>
2025-01-17 20:09:39 +0100

    Setup

--- packages/env/lib/src/env.dev.dart
+++ packages/env/lib/src/env.dev.dart
@@ -5,29 +5,29 @@ part 'env.dev.g.dart';
 /// {@template env}
 /// Dev Environment variables. Used to access environment variables in the app.
 /// {@endtemplate}
-@Envied(path: '.env.dev', obfuscate: true)
+@Envied(path: '.env.dev')
 abstract class EnvDev {
   /// Supabase url secret.
-  @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
+  @EnviedField(varName: 'SUPABASE_URL')
   static String supabaseUrl = _EnvDev.supabaseUrl;
 
   /// Supabase anon key secret.
-  @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
+  @EnviedField(varName: 'SUPABASE_ANON_KEY')
   static String supabaseAnonKey = _EnvDev.supabaseAnonKey;
 
   /// PowerSync ulr secret.
-  @EnviedField(varName: 'POWERSYNC_URL', obfuscate: true)
+  @EnviedField(varName: 'POWERSYNC_URL')
   static String powersyncUrl = _EnvDev.powersyncUrl;
 
   /// Firebase cloud messaging server key secret.
-  @EnviedField(varName: 'FCM_SERVER_KEY', obfuscate: true)
+  @EnviedField(varName: 'FCM_SERVER_KEY')
   static String fcmServerKey = _EnvDev.fcmServerKey;
 
   /// iOS client id key secret.
-  @EnviedField(varName: 'IOS_CLIENT_ID', obfuscate: true)
+  @EnviedField(varName: 'IOS_CLIENT_ID')
   static String iOSClientId = _EnvDev.iOSClientId;
 
   /// Web client id key secret.
-  @EnviedField(varName: 'WEB_CLIENT_ID', obfuscate: true)
+  @EnviedField(varName: 'WEB_CLIENT_ID')
   static String webClientId = _EnvDev.webClientId;
 }
--- packages/env/lib/src/env.prod.dart
+++ packages/env/lib/src/env.prod.dart
@@ -5,29 +5,29 @@ part 'env.prod.g.dart';
 /// {@template env}
 /// Prod Environment variables. Used to access environment variables in the app.
 /// {@endtemplate}
-@Envied(path: '.env.prod', obfuscate: true)
+@Envied(path: '.env.prod')
 abstract class EnvProd {
   /// Supabase url secret.
-  @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
+  @EnviedField(varName: 'SUPABASE_URL')
   static String supabaseUrl = _EnvProd.supabaseUrl;
 
   /// Supabase anon key secret.
-  @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
+  @EnviedField(varName: 'SUPABASE_ANON_KEY')
   static String supabaseAnonKey = _EnvProd.supabaseAnonKey;
 
   /// PowerSync ulr secret.
-  @EnviedField(varName: 'POWERSYNC_URL', obfuscate: true)
+  @EnviedField(varName: 'POWERSYNC_URL')
   static String powersyncUrl = _EnvProd.powersyncUrl;
 
   /// Firebase cloud messaging server key secret.
-  @EnviedField(varName: 'FCM_SERVER_KEY', obfuscate: true)
+  @EnviedField(varName: 'FCM_SERVER_KEY')
   static String fcmServerKey = _EnvProd.fcmServerKey;
 
   /// iOS client id key secret.
-  @EnviedField(varName: 'IOS_CLIENT_ID', obfuscate: true)
+  @EnviedField(varName: 'IOS_CLIENT_ID')
   static String iOSClientId = _EnvProd.iOSClientId;
 
   /// Web client id key secret.
-  @EnviedField(varName: 'WEB_CLIENT_ID', obfuscate: true)
+  @EnviedField(varName: 'WEB_CLIENT_ID')
   static String webClientId = _EnvProd.webClientId;
 }
